9b4dddd48ec9bb4f3a7811d3c225031c7bad871e
karnking <karankotai9183@gmail.com>
2024-05-17 16:09:35 +0530

    cors

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ app.use(cors());
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
         credentials: true
     }
 });
